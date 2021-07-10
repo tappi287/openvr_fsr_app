2b44edd92e207e9368a72c9596bcf27d63d38fae
tappi287 <tapper.stefan@gmail.com>
2021-07-10 22:19:35 +0200

    add info button and collapsible info card

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,52 @@
 <template>
   <div id="main" class="text-left">
-    <h4 class="mt-2">OpenVR FSR App</h4>
+    <div>
+      <b-img width="32px" key="1" src="@/assets/app_logo_inkscape.svg" class="float-left"></b-img>
+      <h4 class="display-inline pl-5 mt-3">OpenVR FSR App</h4>
+    </div>
 
-    <span>Browse through your Steam library, install the OpenVR FSR plugin and adjust the plugin settings.</span>
-    <p>
-      Note: This app has absolutely no knowledge of which Steam app works with the FSR PlugIn.
-      But this app might be the quickest way to find out.
-    </p>
+    <!-- Info -->
+    <b-button size="sm" variant="secondary" v-b-toggle.info-collapse
+              style="position: absolute; top: 0.75rem; right: 0.75rem;">
+      <b-icon icon="info-square-fill" />
+    </b-button>
+    <b-collapse id="info-collapse">
+      <b-card class="setting-card mt-3" bg-variant="dark" text-variant="white" footer-class="pt-0">
+        <p>Browse through your Steam library, let this app install the
+          <b-link href="https://github.com/fholger/openvr_fsr#modified-openvr-dll-with-amd-fidelityfx-superresolution-upscaler"
+                  target="_blank">
+            Modified OpenVR DLL with AMD FidelityFX SuperResolution Upscaler
+          </b-link>
+        and adjust the plugin settings per app.</p>
+        <p>
+          <b>Note:</b> This app has absolutely no knowledge which Steam apps work with the FSR PlugIn.
+          But this app might be the quickest way to find out <b-icon icon="emoji-sunglasses-fill" />
+        </p>
+        <h5>Installation</h5>
+        <p>
+          Hit the <i>install plugin</i> button in the menu for your Steam app of choice.
+          That will enable AMD FidelityFX Super Resolution.
+        </p>
+        <h5>Render Scale</h5>
+        <p>
+          Per-dimension render scale. If smaller than 1 / 100%, will lower the game's render resolution
+          accordingly and afterwards upscale to the "native" resolution set in SteamVR.<br />
+          If greater than 1 / 100%, the game will render at its "native" resolution, and afterwards the
+          image is upscaled to a higher resolution as per the given value.<br />
+          If equals 1 / 100%, effectively disables upsampling, but you'll still get the sharpening stage.<br /><br />
+          AMD presets:<br />
+          Ultra Quality => 0.77<br />
+          Quality       => 0.67<br />
+          Balanced      => 0.59<br />
+          Performance   => 0.50<br />
+        </p>
+      </b-card>
+    </b-collapse>
 
-    <div class="text-center">
+    <!-- Folder -->
+    <div class="text-center mt-3">
       <!-- FSR Folder Select -->
-      <b-button variant="secondary" id="fsr-folder" v-b-popover.hover.auto="'Customize Plugin path.'">
+      <b-button variant="secondary" size="sm" id="fsr-folder" v-b-popover.hover.auto="'Customize PlugIn source path.'">
         <b-icon icon="folder-fill"></b-icon>
       </b-button>
 
@@ -28,7 +64,7 @@
       <!-- Folder Select Popover -->
       <b-popover target="fsr-folder" triggers="click">
         <h5>OpenVR FSR PlugIn Folder</h5>
-        <p>Paste a path in the Format <i>C:\Dir\MyDir</i></p>
+        <p>Paste a path in the Format <i>C:\Dir\MyDir</i> where you want to copy the PlugIn dll from.</p>
         <b-form-input size="sm" v-model="fsrDirInput"
                       placeholder="Paste a folder location">
         </b-form-input>
@@ -38,14 +74,14 @@
             Update
           </b-button>
           <b-button @click="$root.$emit('bv::hide::popover', 'fsr-folder')"
-                    size="sm" aria-label="Close">
+                    size="sm" aria-label="Close" class="ml-2">
             Close
           </b-button>
         </div>
       </b-popover>
     </div>
 
-    <SteamLibTable class="mt-2"></SteamLibTable>
+    <SteamLibTable class="mt-3"></SteamLibTable>
 
     <!-- Busy Overlay -->
     <b-overlay no-wrap fixed :show="isBusy" blur="1px" variant="dark" rounded>
@@ -136,7 +172,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
 #main {
-  width: 97%;
+  width: 96%;
   margin: 0 auto 0 auto;
 }
 </style>
