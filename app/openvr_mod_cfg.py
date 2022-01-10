202a77120425fd3df8e9cbf5febf9741bb7f121f
tappi287 <tapper.stefan@gmail.com>
2022-01-11 00:27:11 +0100

    refactor mod cfg class

--- app/openvr_mod_cfg.py
+++ app/openvr_mod_cfg.py
@@ -1,7 +1,7 @@
 import json
 import logging
 from pathlib import Path
-from typing import List
+from typing import List, Iterator, Dict, Any
 
 from app.globals import OPEN_VR_FSR_CFG
 from app.utils import JsonRepr
@@ -34,7 +34,7 @@ class OpenVRModSettings(JsonRepr):
         self.options = options
         self.cfg_key = cfg_key
 
-    def get_setting_fields(self):
+    def get_setting_fields(self) -> List[str]:
         options = list()
         for attr_name in dir(self):
             field = getattr(self, attr_name)
@@ -42,22 +42,22 @@ class OpenVRModSettings(JsonRepr):
                 options.append(attr_name)
         return options
 
-    def _get_options(self):
+    def _get_options(self) -> Iterator[OpenVRModCfgSetting]:
         for key in self.options:
             option = getattr(self, key)
             if not option.parent:
                 yield option
 
-    def _get_all_options(self):
+    def _get_all_options(self) -> Iterator[OpenVRModCfgSetting]:
         for key in self.options:
             yield getattr(self, key)
 
-    def _get_option_by_key(self, key, parent_key=None):
+    def _get_option_by_key(self, key, parent_key=None) -> OpenVRModCfgSetting:
         for option in self._get_all_options():
             if option.parent == parent_key and key == option.key:
                 return option
 
-    def _get_parented_options(self) -> dict:
+    def _get_parented_options(self) -> Dict[str, Dict[str, Any]]:
         parented_options = dict()
         for o in self._get_all_options():
             if o.parent:
@@ -74,32 +74,13 @@ class OpenVRModSettings(JsonRepr):
             return False
 
         try:
-            with open(cfg, 'r') as f:
-                # -- Remove Inline Comments
-                json_str = str()
-                for line in f.readlines():
-                    if '//' not in line and '#' not in line:
-                        json_str += line
+            json_dict = self._cfg_to_json(cfg)
 
-                json_dict = json.loads(json_str)
+            # -- Check if the cfg key e.g. 'fsr' is defined inside the CFG
+            if self.cfg_key not in json_dict:
+                return False
 
-                # -- Check if the cfg key e.g. 'fsr' is defined inside the CFG
-                if self.cfg_key not in json_dict:
-                    return False
-
-                for s in self._get_options():
-                    json_value = json_dict[self.cfg_key].get(s.key)
-                    if json_value is not None:
-                        s.value = json_value
-
-                # -- Read parented options
-                parented_opts = self._get_parented_options()
-                for parent_key, settings_dict in parented_opts.items():
-                    for key, value in settings_dict.items():
-                        json_value = json_dict[self.cfg_key].get(parent_key, dict()).get(key)
-                        if json_value is not None:
-                            option = self._get_option_by_key(key, parent_key)
-                            option.value = json_value
+            self.from_json(json_dict[self.cfg_key])
         except Exception as e:
             logging.error('Error reading FSR settings from cfg file: %s', e)
             return False
@@ -131,20 +112,48 @@ class OpenVRModSettings(JsonRepr):
             return False
         return True
 
+    @staticmethod
+    def _cfg_to_json(cfg_file: Path) -> dict:
+        try:
+            with open(cfg_file, 'r') as f:
+                # -- Remove Inline Comments
+                json_str = str()
+                for line in f.readlines():
+                    if '//' not in line and '#' not in line:
+                        json_str += line
+
+                return json.loads(json_str)
+        except Exception as e:
+            logging.error(f'Error reading CFG file {cfg_file}: {e}')
+
+        return dict()
+
     def to_js(self, export: bool = False) -> list:
         return [s.to_js_object(export) for s in self._get_all_options()]
 
-    def to_ini_js(self):
+    def to_ini_js(self) -> dict:
         options = {s.key: s.value for s in self._get_options()}
         options.update(self._get_parented_options())
 
         return {self.cfg_key: options}
 
-    def from_js_dict(self, json_list):
-        if not json_list:
+    def from_json(self, settings: dict):
+        for key, value in settings.items():
+            if isinstance(value, dict):
+                for child_key, child_v in value.items():
+                    option = self._get_option_by_key(key, child_key)
+                    if option:
+                        option.value = child_v
+            else:
+                option = self._get_option_by_key(key)
+                if option:
+                    option.value = value
+
+    def from_js_dict(self, js_object_list: list):
+        if not js_object_list:
             return
 
-        for s in json_list:
+        for s in js_object_list:
             option = self._get_option_by_key(s.get('key'), s.get('parent'))
             if option:
                 option.value = s.get('value')
