5991a60c629887ec8aab42e678a2de744011b434
tappi287 <tapper.stefan@gmail.com>
2022-01-23 01:29:32 +0100

    add BaseModCfgType to __all__

--- app/cfg/__init__.py
+++ app/cfg/__init__.py
@@ -1,7 +1,7 @@
-from .base_mod_cfg import BaseModCfgSetting, BaseModSettings
+from .base_mod_cfg import BaseModCfgSetting, BaseModSettings, BaseModCfgType
 from .fsr_cfg import FsrSettings
 from .foveated_cfg import FoveatedSettings
 from .vrperfkit_cfg import VRPerfKitSettings
 
-__all__ = ['BaseModCfgSetting', 'BaseModSettings',
+__all__ = ['BaseModCfgSetting', 'BaseModSettings', 'BaseModCfgType',
            'FsrSettings', 'FoveatedSettings', 'VRPerfKitSettings']
