d33516278820df12eb26caeb90e03f7204f4bf4a
tappi287 <tapper.stefan@gmail.com>
2022-01-30 10:44:39 +0100

    adapt new install path handling

--- tests/test_app/test_vrperfkit_mod.py
+++ tests/test_app/test_vrperfkit_mod.py
@@ -21,26 +21,25 @@ def _create_test_output(output_path, vrperfkit_dir):
     return test_dxgi_dll, test_mod_yml_path
 
 
-def test_update_mod_fn(test_app, output_path, vrperfkit_dir):
-    # -- Create test output
-    _vrp_dll_out, _vrp_mod_cfg_out = _create_test_output(output_path, vrperfkit_dir)
+def test_update_mod_fn(test_app_writeable, vrperfkit_dir):
+    t = test_app_writeable
 
-    # -- Update Test App
-    test_app['executablePaths'] = [str(_vrp_dll_out.parent / 'SomeNoneExistingBinary.exe')]
-    # test_app['executablePathsSelected'] = test_app['executablePaths']
+    # -- Create test output
+    _vrp_dll_out, _vrp_mod_cfg_out = _create_test_output(Path(t.get('path')), vrperfkit_dir)
+    t[VRPerfKitMod.DLL_LOC_KEY_SELECTED] = [Path(_vrp_dll_out.parent / 'Another Binary.exe').as_posix()]
 
     # -- Create Test Settings
     test_set = ([('method', 'upscaling'), ('renderScale', 'upscaling'), ('debugMode', None),
                  ('cycleUpscalingMethod', 'hotkeys')],
                 ['nis', 2.50, True, ["shift", "l"]])
-    test_app, test_settings = create_manipulated_settings(test_app, test_set, app.mod.get_mod(dict(), BaseModType.vrp))
+    t, test_settings = create_manipulated_settings(t, test_set, app.mod.get_mod(dict(), BaseModType.vrp))
 
     # -- Test Fn
-    result_dict = json.loads(app_fn.update_mod_fn(test_app, BaseModType.vrp, True))
+    result_dict = json.loads(app_fn.update_mod_fn(t, BaseModType.vrp, True))
 
     # -- Reset settings and read settings back
-    test_app.pop(VRPerfKitMod.VAR_NAMES['settings'])
-    mod = VRPerfKitMod(test_app)
+    t.pop(VRPerfKitMod.VAR_NAMES['settings'])
+    mod = VRPerfKitMod(t)
     mod.update_from_disk()
 
     # -- Results
@@ -57,7 +56,7 @@ def test_update_mod_fn(test_app, output_path, vrperfkit_dir):
                 assert setting.get('value') == _test_value
 
     # -- Verify written file matches settings
-    for exe_loc in test_app['executablePathsSelected']:
+    for exe_loc in t['executablePathsSelected']:
         cfg = Path(exe_loc).parent / mod.settings.CFG_FILE
         yml_data = ModCfgYamlHandler.read_cfg(mod.settings, cfg)
         for _test_s in test_settings:
